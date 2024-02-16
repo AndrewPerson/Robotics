2272002ef99352816880e470a0ec06a58b4c8071
AndrewPerson <professor_smart@hotmail.com>
2024-02-16 11:03:36 +1100

    Add ultrasound

---
+++ ultrasound.ino
@@ -0,0 +1,56 @@
+const int trigPin = 9;
+const int echoPin = 10;
+const int LEDPin = 3;
+
+const float speedOfSound = 0.0343; // Speed of sound in cm/micro second
+
+// Returns in centimetres
+long getUltrasoundDistance(int trigPin, int echoPin)
+{
+  long duration;
+
+  do
+  {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(15);
+    digitalWrite(trigPin, LOW);
+  
+    duration = pulseIn(echoPin, HIGH);
+  }
+  while (duration == 0);
+
+  long cm = duration / 2 * speedOfSound;
+  
+  return cm;
+}
+
+bool getSwitchPressed(int pin)
+{
+  
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+ 
+  pinMode(LEDPin, OUTPUT);
+}
+
+void loop()
+{
+  auto distance = getUltrasoundDistance(trigPin, echoPin);
+  
+  Serial.println(distance);
+  
+  // if (distance <= 20) {
+  //   digitalWrite(LEDPin, HIGH);
+  // }
+  // else {
+  //   digitalWrite(LEDPin, LOW);
+  // }
+}
